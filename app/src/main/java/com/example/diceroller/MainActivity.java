0120a2e9922766bda7bfcf8cd32678893aa587d7
catarina <kate.silva@icloud.com>
2020-02-09 20:26:25 +0000

    Level 2: Add input box to enter number, display Congratulations

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -17,10 +17,33 @@ public class MainActivity<rollTheDiceButton> extends AppCompatActivity {
 
     Button rollDiceButton;
 
+    int enterNumber;
+
+    TextView numberInputted;
+
+    EditText numberInputEditText;
+
+    Button submitNumberButton;
+
+    int UserNumber;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        submitNumberButton = (Button) findViewById(R.id.submitButton);
+        submitNumberButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberInputEditText = (EditText) findViewById(R.id.numberInput);
+                UserNumber = Integer.valueOf(numberInputEditText.getText().toString());
+                numberInputted = (TextView) findViewById(R.id.numberInputtedTextView);
+                numberInputted.setText("You entered: " + UserNumber);
+
+                Log.d("DiceRoller", "Your entered number is: " + UserNumber);
+            }
+            });
 
         rollDiceButton = (Button) findViewById(R.id.rollTheDiceButton);
 
@@ -34,6 +57,16 @@ public class MainActivity<rollTheDiceButton> extends AppCompatActivity {
                 int max = 6;
                 int number = r.nextInt((max - min)+1)+min;
                 randomNumber.setText(Integer.toString(number));
+                TextView congratsMessage;
+                congratsMessage = (TextView) findViewById(R.id.congratulationsTextView);
+                if(UserNumber == number)
+                {
+                    congratsMessage.setText("Congratulations!");
+                }
+                else
+                {
+                    congratsMessage.setText("Unlucky :(");
+                }
 
             }
         });
