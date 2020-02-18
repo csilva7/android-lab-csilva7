41e366d867579dfd7da2290d43d0335be199788c
catarina <kate.silva@icloud.com>
2020-02-18 21:52:30 +0000

    Level 3

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 import android.widget.EditText;
 import java.util.Random;
 
+
 public class MainActivity<rollTheDiceButton> extends AppCompatActivity {
 
     Button rollDiceButton;
@@ -26,6 +27,8 @@ public class MainActivity<rollTheDiceButton> extends AppCompatActivity {
     Button submitNumberButton;
 
     int UserNumber;
+
+    int Score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,34 +46,29 @@ public class MainActivity<rollTheDiceButton> extends AppCompatActivity {
 
                 Log.d("DiceRoller", "Your entered number is: " + UserNumber);
             }
-            });
-
-        rollDiceButton = (Button) findViewById(R.id.rollTheDiceButton);
-
-        rollDiceButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView randomNumber;
-                randomNumber = (TextView) findViewById(R.id.randomNumberTextView);
-                Random r = new Random();
-                int min = 1;
-                int max = 6;
-                int number = r.nextInt((max - min)+1)+min;
-                randomNumber.setText(Integer.toString(number));
-                TextView congratsMessage;
-                congratsMessage = (TextView) findViewById(R.id.congratulationsTextView);
-                if(UserNumber == number)
-                {
-                    congratsMessage.setText("Congratulations!");
-                }
-                else
-                {
-                    congratsMessage.setText("Unlucky :(");
-                }
-
-            }
         });
 
-
     }
+
+        public void on_button_click(View v) {
+            TextView randomNumber;
+            randomNumber = (TextView) findViewById(R.id.randomNumberTextView);
+            Random r = new Random();
+            int min = 1;
+            int max = 6;
+            int number = r.nextInt((max - min) + 1) + min;
+            randomNumber.setText(Integer.toString(number));
+            TextView congratsMessage;
+            congratsMessage = (TextView) findViewById(R.id.congratulationsTextView);
+            if (UserNumber == number) {
+                congratsMessage.setText("Congratulations!");
+                Score = Score + 1;
+                TextView currentScore;
+                currentScore = (TextView) findViewById(R.id.scoreTextView);
+                currentScore.setText(Integer.toString(Score));
+                Log.d("DiceRoller", "Your score is: " + Score);
+            } else {
+                congratsMessage.setText("Unlucky :(");
+            }
+        }
 }
